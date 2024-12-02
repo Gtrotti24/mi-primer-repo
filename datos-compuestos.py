0552646050d1f01291c8a99f893a7ec087345e43
Gtrotti <144169722+Gtrotti24@users.noreply.github.com>
2024-12-02 19:59:51 -0300

    datos-compuestos.py
    datos compuestos de python

---
+++ datos-compuestos.py
@@ -0,0 +1,15 @@
+#creando una lista ( se pueden modificar)
+lista = ["lucas dato", "soy dalto", True, 1.85]
+
+#creando una tupla (no se puede modificar)
+tupla = ("lucas dalto", "soy dalto", True, 1.85)
+
+#Esto es valido
+lista[3] = "maquinola"
+
+#esto no es valido
+#tupla[3] = "maquinola"
+
+#creando un conjunto (set)
+
+
